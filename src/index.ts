1be5456a35e660ddabbd609239b99d7cbfa01b1e
aoewe <2022449709@qq.com>
2024-05-23 10:33:28 +0800

    +

--- src/index.ts
+++ src/index.ts
@@ -86,4 +86,4 @@ app.use('', router)
 app.use('/api', router)
 app.set('trust proxy', 1)
 
-app.listen(1002, () => globalThis.console.log('Server is running on port 1002'))
+app.listen(3000, () => globalThis.console.log('Server is running on port 3000'))
